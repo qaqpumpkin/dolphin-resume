1ff4228900c5f97e08dcb83eb7680ba802fc1cc2
chennanying <chennanying@joyy.com>
2022-08-13 21:01:19 +0800

    feat: 简历模板部分架构

--- app/renderer/container/resume/ResumeContent/BaseTemplate/index.tsx
+++ app/renderer/container/resume/ResumeContent/BaseTemplate/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Avatar from './components/Avatar';
+import BaseInfo from './components/BaseInfo';
 import './index.less';
+import Skill from './components/Skill';
 
 function BaseTemplate() {
-  return <div>123</div>;
+  return (
+    <div styleName="resume">
+      <div styleName="title">
+        <div styleName="baseInfo">
+          <BaseInfo />
+        </div>
+        <div styleName="avatar">
+          <Avatar />
+        </div>
+      </div>
+      <div styleName="resumeData">
+        <Skill />
+      </div>
+    </div>
+  );
 }
 
 export default BaseTemplate;
